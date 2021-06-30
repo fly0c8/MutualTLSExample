d19fb8daa695a6358b07db030e500038a4b32367
Arnold Angerer <arnold.angerer@gmail.com>
2021-06-30 08:50:44 +0200

    blah

--- gen_certs.sh
+++ gen_certs.sh
@@ -1,11 +1,3 @@
-# openssl  req -newkey rsa:2048 \
-# 	-new -nodes -x509 \
-# 	-days 3650 \
-# 	-out cert.pem \
-# 	-keyout key.pem \
-# 	-subj "/C=AT/ST=Salzburg/L=Koppl/O=zerobyte/OU=hq/CN=localhost"
-# 	-addext "subjectAltName = DNS:reactive.at"
-
 openssl req -x509 -newkey rsa:4096 -sha256 -days 3650 -nodes \
   -keyout key.pem -out cert.pem -subj '/C=AT/ST=Salzburg/L=Koppl/O=zerobyte/OU=hq/CN=localhost' \
   -addext 'subjectAltName=DNS:localhost,DNS:example.com,DNS:example.net'
