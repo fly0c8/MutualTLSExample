aafae35e9bd7aba1831545550f82cf7e827763fa
Arnold Angerer <arnold.angerer@gmail.com>
2021-06-30 08:48:09 +0200

    first

---
+++ client.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"crypto/tls"
+	"crypto/x509"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+)
+
+func main() {
+
+	// create a ca certificate pool and add cert.pem to it
+	caCert, err := ioutil.ReadFile("cert.pem")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	caCertPool := x509.NewCertPool()
+	caCertPool.AppendCertsFromPEM(caCert)
+
+	// Read the key pair to create certificate
+	cert, err := tls.LoadX509KeyPair("cert.pem", "key.pem")
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	client := &http.Client{
+		Transport: &http.Transport{
+			TLSClientConfig: &tls.Config{
+				RootCAs:      caCertPool,
+				Certificates: []tls.Certificate{cert},
+			},
+		},
+	}
+
+	r, err := client.Get("https://localhost:8443/hello")
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Read the response body
+	defer r.Body.Close()
+	body, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Print the response body to stdout
+	fmt.Printf("%s\n", body)
+}
---
+++ server.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"crypto/tls"
+	"crypto/x509"
+	"io"
+	"io/ioutil"
+	"log"
+	"net/http"
+)
+
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+	// Write "Hello, world!" to the response body
+	io.WriteString(w, "Hello, world!\n")
+}
+
+func main() {
+	// Set up a /hello resource handler
+	http.HandleFunc("/hello", helloHandler)
+
+	caCert, err := ioutil.ReadFile("cert.pem")
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	caCertPool := x509.NewCertPool()
+	caCertPool.AppendCertsFromPEM(caCert)
+	tlsConfig := &tls.Config{
+		ClientCAs:  caCertPool,
+		ClientAuth: tls.RequireAndVerifyClientCert,
+	}
+	tlsConfig.BuildNameToCertificate()
+	server := &http.Server{
+		Addr:      ":8443",
+		TLSConfig: tlsConfig,
+	}
+
+	// Listen to port 8080 and wait
+	log.Fatal(server.ListenAndServeTLS("cert.pem", "key.pem"))
+
+}
